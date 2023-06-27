cb6ef526e0930c5309ec1388245600fb65b89507
Stef Boerjan <stef.boerjan@gmail.com>
2023-06-27 16:01:30 +0200

    list of chores from backend

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,10 @@
+function Card({title, description}: any) {
+  return (
+    <div>
+        <li>{title}</li>
+        <li>{description}</li>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/OverviewComponent/OverviewComponent.tsx
+++ src/components/OverviewComponent/OverviewComponent.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import './OverviewComponent.css';
+import { Chore } from '../../models/Chore';
+import Card from '../Card/Card';
 
 function OverviewComponent() {
-  
+  const [chores, setChores] = useState<Chore[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:5013/api/Chores")
+    .then((res) => res.json())
+    .then((data) => {
+      setChores(data);
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+  }, []);
+
+  const listItems = chores.map(chore => <Card title={chore.title} description={chore.description} />);
+
   return (
     <div>
         <div className='title'>Welcome Stef</div>
         <div className='subtitle'>Your chores</div>
+        <div>{listItems}</div>
     </div>
   );
 }
