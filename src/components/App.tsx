15628417a7c5cea51588a815871ecf770a3f8e5d
Stef Boerjan <stef.boerjan@gmail.com>
2023-06-27 12:54:47 +0200

    folder structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import logo from '../assets/logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5013/WeatherForecast')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setData(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
